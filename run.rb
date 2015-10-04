8a12fc6fe399ce98d4711b60b2753177b73822ee
Kevin Boyd <kevinisverycool@yahoo.com>
2015-10-04 19:05:12 -0400

    commit 1

--- run.rb
+++ run.rb
@@ -9,5 +9,19 @@ end
 
 get '/states' do
   # code!
+  @states = []
+  state = {id: 'NJ', name: 'New Jersey'}
+  @states << state
+  state = {id: 'MI', name: 'Michigan'}
+  @states << state
+  state = {id: 'NV', name: 'Nevada'}
+  @states << state
+  state = {id: 'NH', name: 'New Hampshire'}
+  @states << state
+  state = {id: 'WA', name: 'Washington'}
+  @states << state
+  
+  @states.sort_by! { | state | state[:name] } 
+  
   erb :states, layout: :main
 end
